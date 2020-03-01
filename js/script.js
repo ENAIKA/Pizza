3d9551113f0330e9c004ca41622b6cf9d76b28fc
ENAIKA <naikaesther5@gmail.com>
2020-03-01 22:27:15 +0300

    declare variables that hold user input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 //Constructor
 function Order(number,size,crust,topping){
     this.number=number;
@@ -17,3 +15,17 @@ var crust={butter:100, garlicButter:150, canjumButter:200, parmesan:100, sesameS
 
 //topping objects
 var topping={chicken:100, bacon:150, bbq:200, tomatoes:250,onions:50,olives:300, meatballs:100, pineapple:150, mushroom:300, cheese:100}
+//function to get values from multiple selection
+function getMultiple() {
+    var selectElement = document.getElementById("topping");
+    var selectedElements = [];
+    for (var i = 0; i < selectElement.length; i++) {
+        if (selectElement.options[i].selected) selectedElements.push(selectElement.options[i].value);
+    }
+   return selected1;
+}
+//declare variables
+var getValues=getMultiple();
+var inputtedNumber=$("input#num").val();
+var selectedSize=$("select#size option:selected").val();
+var selectedCrust=$("select#crust option:selected").val();
