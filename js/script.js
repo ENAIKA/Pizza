b07a061ca33f208fd4c0b1513cd9a49a2bab7c07
ENAIKA <naikaesther5@gmail.com>
2020-03-01 18:15:26 +0300

    change Order Constructor and add objects size, crust, topping

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 //Constructor
 function Order(number,size,crust,topping){
     this.number=number;
-    this.size=size;
-    this.crust=crust;
-    this.topping=topping;
+    this.size=[];
+    this.crust=[];
+    this.topping=[];
 }
+//size objects
+
+var size={small:250, medium:350, large:450}
+
+//crust objects
+
+var crust={butter:100, garlicButter:150, canjumButter:200, parmesan:100, sesameSeed:150, everything:250}
+
+//topping objects
+var topping={chicken:100, bacon:150, bbq:200, tomatoes:250,onions:50,olives:300, meatballs:100, pineapple:150, mushroom:300, cheese:100}
