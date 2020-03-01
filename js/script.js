e30d5b95f41ee338976fb66423f3b1cf06855230
ENAIKA <naikaesther5@gmail.com>
2020-03-02 00:08:36 +0300

    create method to order constructor

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 //Constructor
-function Order(number,size,crust,topping){
+function Order(number){
     this.number=number;
     this.size=[];
     this.crust=[];
     this.topping=[];
 }
-//size objects
+//size object
 
 var size={small:250, medium:350, large:450}
 
-//crust objects
+//crust object
 
 var crust={butter:100, garlicButter:150, canjumButter:200, parmesan:100, sesameSeed:150, everything:250}
 
-//topping objects
+//topping object
 var topping={chicken:100, bacon:150, bbq:200, tomatoes:250,onions:50,olives:300, meatballs:100, pineapple:150, mushroom:300, cheese:100}
-//function to get values from multiple selection
+
+//function to get values from multiple selection #topping
 function getMultiple() {
     var selectElement = document.getElementById("topping");
     var selectedElements = [];
@@ -29,3 +30,9 @@ var getValues=getMultiple();
 var inputtedNumber=$("input#num").val();
 var selectedSize=$("select#size option:selected").val();
 var selectedCrust=$("select#crust option:selected").val();
+var newOrder= new Order(inputtedNumber, selectedSize, selectedCrust, selectedTopping);
+//method
+Order.prototype.fullorder=function(){
+    var fullorder=(size[selectedSize]+crust[selectedCrust]+topping[selectedTopping])*this.number;
+    return fullorder;
+}
