0e74e18a8fc38755546c53165478b3cb21de7b3b
ENAIKA <naikaesther5@gmail.com>
2020-03-04 12:01:16 +0300

    code refactor remove get multiple function

--- js/script.js
+++ js/script.js
@@ -5,51 +5,65 @@ function Order(number, size, crust, topping){
     this.crust=crust;
     this.topping=topping;
 }
+Order.prototype.fullorder=function(){
+    var fullorder=((this.size +this.crust+this.topping)*this.number); 
+    return fullorder;
+}
 //size object
 
-var sizes={small:250, medium:350, large:450}
+var sizes={small:250, medium:350, large:450};
 
 //crust object
 
-var crusts={butter:100, garlicButter:150, canjumButter:200, parmesan:100, sesameSeed:150, everything:250}
+var crusts={butter:100, garlicButter:150, canjumButter:200, parmesan:100, sesameSeed:150, everything:250};
 
 //topping object
-var topping={chicken:100, bacon:150, bbq:200, tomatoes:250,onions:50,olives:300, meatballs:100, pineapple:150, mushroom:300, cheese:100}
+var toppings={chicken:100, bacon:150, bbq:200, tomatoes:250,onions:50,olives:300, meatballs:100, pineapple:150, mushroom:300, cheese:100};
 
 //function to get values from multiple selection #topping
-function getMultiple() {
-    var selectElement = document.getElementById("topping");
-    var selectedElements = [];
-    for (var i = 0; i < selectElement.length; i++) {
-        if (selectElement.options[i].selected) selectedElements.push(selectElement.options[i].value);
-    }
-   return selectedElements;
-}
-//declare variables
-var getValues=getMultiple();
-//an array of prices from get value
-function getPrices(price) {
-   return price.map(function (item, index) {
-        return topping[getValues[index]];
 
-    })
+// function getMultiple() {
+//     var selectElement = document.getElementById("topping");
+//     var selectedElements = [];
+//     for (var i = 0; i < selectElement.length; i++) {
+//         if (selectElement.options[i].selected) selectedElements.push(selectElement.options[i].value);
+//     }
+//    return selectedElements;
+// }
+//declare variables
+
+//an array of prices from getValue()
+function getPrices(price) {
+   return price.map(function (item) {
+        return toppings[item];
+
+    });
 }
-var prices=getPrices(getValues)
+
 //reduce the array
 function reducePrices(){
     var reducer=(acc, currentValue)=> acc+currentValue;
    return prices.reduce(reducer);
 }
+$(document).ready(function () {
+    $("#btnOrder").click(function () {
+        event.preventDefault();
+        prices=getPrices($("select#topping").val());
+        priced=$("select#topping").val();
+        var selectedTopping = reducePrices();//should be halved maybe to get reasonable prices
 
-var selectedTopping=reducePrices();//should be halved maybe to get reasonable prices
+        var inputtedNumber = parseInt($("input#num").val());
+        var selectedSize = $("select#size option:selected").val();
+        var selectedCrust = $("select#crust option:selected").val();
+        var newOrder = new Order(inputtedNumber, sizes[selectedSize], crusts[selectedCrust], selectedTopping);
 
-var inputtedNumber=parseInt($("input#num").val());
-var selectedSize=$("select#size option:selected").val();
-var selectedCrust=$("select#crust option:selected").val();
-var newOrder= new Order(inputtedNumber, sizes[selectedSize], crusts[selectedCrust], selectedTopping);
+        $("ul#getMyOrder").append("<li> <span class='getMyOrder'>" + inputtedNumber +" " +selectedSize +"-"+"sized" +" "+ selectedCrust +" "+"crust" +" "+ "with"+" "+priced+ " "+"topping pizza"+ " " +newOrder.fullorder()+"</span></li>");
+    });
+});
 
 //method
-Order.prototype.fullorder=function(){
-    var fullorder=(this.size +this.crust+this.topping)*this.number; 
-    return fullorder;
-}
+//method to get whole billing
+// Order.prototype.totalOrder=function(){
+//     var array1=[];
+//     for (item in )
+// }
